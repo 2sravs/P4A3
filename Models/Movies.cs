31e292ded4c3ccec8510ab361c2a349e53688a5e
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-21 22:32:01 +0530

    firstcommit

---
+++ Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment3.Models
+{
+    public class Movies
+    {
+
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+    }
+}
+
+
